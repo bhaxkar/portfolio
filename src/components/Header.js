9f273100b03f19a1fb1fff467fdf1167db19d0cf
bhaxkar0 <hlobhaskarjha02@gmail.com>
2025-03-09 11:03:26 +0530

    updating

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
             
             <div className="mt-12 md:mt-16 w-11/12 md:w-6/12 mx-auto backdrop-blur-sm bg-black/30 p-4 md:p-6 rounded-xl shadow-xl">
                 <div>
-                    <h1 className="text-custom-yellow font-coro text-2xl md:text-3xl mb-2 text-center md:text-left">👋 Hey, I'm Bhaskar Jha</h1>
+                    <h1 className="text-custom-yellow font-coro text-3xl md:text-3xl mb-2 text-center md:text-left">👋 Hey, I'm Bhaskar Jha</h1>
                 </div>
                 
                 <div className="flex justify-center md:justify-start md:mx-12 gap-4 mt-4">
@@ -32,9 +32,9 @@ const Header = () => {
                 <div className="text-custom-yellow mt-6">
                     <p className="text-xl md:text-2xl mb-4 font-mono text-left md:text-left">Right Now, I'm</p>
                     <ul className="space-y-4 font-mono">
-                       <li className="text-lg md:text-base">➢ Honing my technical acumen in Information Technology at <a href="https://hithaldia.ac.in/" className="underline hover:text-red-200 transition-colors">Haldia Institute of Technology</a>.</li>
-                       <li className="text-lg md:text-base">➢ Working as <span className="font-bold">SDE Intern</span> at <a href="https://www.turnpikeanalyst.com/" className="underline hover:text-red-200 transition-colors">Turnpike Analyst</a>, contributing to innovative solutions.</li>
-                       <li className="text-lg md:text-base">➢ Spearheading technical initiatives as the <a href="https://www.instagram.com/acmhitchapter/" className="underline hover:text-red-200 transition-colors">ACM HIT Student Chapter's</a> <span className="font-bold">Tech Lead</span>.</li>
+                       <li className="text-lg md:text-xl">➢ Honing my technical acumen in Information Technology at <a href="https://hithaldia.ac.in/" className="underline hover:text-red-200 transition-colors">Haldia Institute of Technology</a>.</li>
+                       <li className="text-lg md:text-xl">➢ Working as <span className="font-bold">SDE Intern</span> at <a href="https://www.turnpikeanalyst.com/" className="underline hover:text-red-200 transition-colors">Turnpike Analyst</a>, contributing to innovative solutions.</li>
+                       <li className="text-lg md:text-xl">➢ Spearheading technical initiatives as the <a href="https://www.instagram.com/acmhitchapter/" className="underline hover:text-red-200 transition-colors">ACM HIT Student Chapter's</a> <span className="font-bold">Tech Lead</span>.</li>
                     </ul>
                 </div>
             </div>
