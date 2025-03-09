f8cca3709b84b2b2f19683656a713092e9689069
bhaxkar0 <hlobhaskarjha02@gmail.com>
2025-03-09 10:51:02 +0530

    updating ui

---
+++ src/components/about.js
@@ -0,0 +1,21 @@
+const About = () =>{
+    return(
+        <div className="pt-6 md:pt-8 text-custom-yellow min-h-screen p-2 md:p-4">
+            <h2 className="text-xl md:text-2xl font-mono text-center md:text-right mb-4 md:mb-6">Who am I?</h2>
+            <div className="space-y-3 md:space-y-4">
+                <p className="font-mono text-sm md:text-base">
+                I'm Bhaskar Jha, a tech enthusiast with a solid foundation in Computer Science. I have skills in MERN stack, Core Java (OOPs, multithreading, collection framework, and exception handling), Golang (concurrent programming), Python, SQL, AWS and I'm actively exploring the exciting domain of AI & ML.
+                </p>
+                <p className="font-mono text-sm md:text-base">
+                I've successfully tackled a variety of coding challenges, which have enhanced my problem-solving skills.
+                </p>
+                <p className="font-mono text-sm md:text-base">
+                I'm Seeking an SDE Internship to contribute to groundbreaking projects. Let's build something amazing together! 
+                </p>
+                <p className="font-mono text-sm md:text-base">
+                Feel free to reach out to me via email at bhaskarjha.info@gmail.com 
+                </p>
+            </div>
+        </div>
+    )
+}
