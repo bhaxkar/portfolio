d85505020c56e766f71d0c657aa0495376a7a1ec
bhaxkar0 <hlobhaskarjha02@gmail.com>
2025-03-10 11:19:41 +0530

    updating ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Resume from "../../public/resume.pdf"
 const Header = () => {
   return (
     <div className="absolute left-1/2 transform -translate-x-1/2 w-full py-3">
-      <nav className="flex justify-center space-x-6 md:space-x-12">
+      <nav className="flex justify-center space-x-8 md:space-x-12">
         <Link
           to="/"
           className="text-custom-yellow font-mono text-base md:text-lg font-semibold hover:text-red-200 hover:scale-110 cursor-pointer transition-all duration-300"
